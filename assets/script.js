8ce0784cfdd56c0be935c23db0916d494bf3a25c
Isaiah411013 <Archangel_r320@hotmail.com>
2022-09-17 10:47:43 -0700

    updating starter code

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,35 @@ WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist*/
+
+const openEl = document.getElementById('open');
+const el1 = document.getElementById('el1');
+const el2 = document.getElementById('el2');
+const el3 = document.getElementById('e3');
+const currentDay = document.getElementsByClassName('lead');
+const el4 = document.getElementById('el4');
+// openEl.addEventListener('click', planner);
+
+// function planner(){
+//     openEl.classList.add('hide');
+//     el1.classList.remove('hide');
+//     el2.classList.remove('hide');
+//     el3.classList.remove('hide');
+//     el1.currentDay.remove('hide');
+//     el4.classList.remove('hide');
+//     upDateTime()
+// }
+
+//this portion of the time format sets time instantly on the page
+const time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(time);
+
+function upDateTime(){
+const time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(time);
+}
+// the interval recalss the function upDateTime every second keepingthe time current.
+setInterval(upDateTime,1000);
+
+
+
